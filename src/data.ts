027b463a002469e0deee2e90e86fa267d407eb98
kennedy7 <kennedyhillary6@gmail.com>
2023-05-27 14:07:00 +0100

    data interface

--- src/data.ts
+++ src/data.ts
@@ -1,3 +1,15 @@
 const data = {
     report: []
 }
+
+interface Data{
+    report: {
+        id: string;
+        source: string;
+        amount: number;
+        createdAt: Date;
+        updateddAt: Date;
+        type: string;
+
+    }
+}
