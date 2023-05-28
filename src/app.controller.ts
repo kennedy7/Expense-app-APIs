ad8bd4f1a993ed7135b0d2618d280cd5c44113ed
kennedy7 <kennedyhillary6@gmail.com>
2023-05-28 11:16:25 +0100

    getReportById

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { data, ReportType } from './data';
 
-@Controller()
+@Controller('report/:type')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('report/:type')
+  @Get()
   getAllReports(@Param('type') type: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
