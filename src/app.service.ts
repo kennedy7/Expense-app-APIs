be6491bee612ed5340d191e174b9e2e89fba6d53
kennedy7 <kennedyhillary6@gmail.com>
2023-05-30 12:05:22 +0100

    refactor

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,10 @@
 import { Injectable, Param } from '@nestjs/common';
 import { data, ReportType } from './data';
 
+interface ReportDto {
+  amount: number;
+  source: string;
+}
 @Injectable()
 export class AppService {
   getAllReports(type: ReportType) {
@@ -11,4 +15,5 @@ export class AppService {
       .filter((report) => report.type === type)
       .find((report) => report.id === id);
   }
+  createReport(type: ReportType, data: ReportDto) {}
 }
