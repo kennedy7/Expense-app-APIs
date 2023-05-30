b2744c88739c060315ab45903306dc750167dd95
kennedy7 <kennedyhillary6@gmail.com>
2023-05-30 12:09:17 +0100

    refactor

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Param } from '@nestjs/common';
 import { data, ReportType } from './data';
+import { v4 as uuid } from 'uuid';
 
 interface ReportDto {
   amount: number;
@@ -15,5 +16,17 @@ export class AppService {
       .filter((report) => report.type === type)
       .find((report) => report.id === id);
   }
-  createReport(type: ReportType, data: ReportDto) {}
+  createReport(type: ReportType, { amount, source }: ReportDto) {
+    const newReport = {
+      id: uuid(),
+      amount,
+      source,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+
+    data.report.push(newReport);
+    return newReport;
+  }
 }
