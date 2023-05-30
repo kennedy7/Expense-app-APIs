7ba5c6ca89c0814109a87f412bc6b565820d23c0
kennedy7 <kennedyhillary6@gmail.com>
2023-05-30 11:48:22 +0100

    refactoring business logic to service module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { data, ReportType } from './data';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
+export class AppService {}
