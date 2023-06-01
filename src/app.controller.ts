f77d69fd03b19dc49577e646c9ca0c9dc629f4c6
kennedy7 <kennedyhillary6@gmail.com>
2023-06-01 16:56:13 +0100

    UpdateReportDto

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -12,7 +12,7 @@ import { AppService } from './app.service';
 import { ReportType } from './data';
 import { ReportTypeValidationPipe } from './pipes/report-type-validation.pipe';
 import { Delete, HttpCode } from '@nestjs/common/decorators';
-import { CreateReportDto } from './dtos/report.dto';
+import { CreateReportDto, UpdateReportDto } from './dtos/report.dto';
 
 @Controller('report/:type')
 export class AppController {
@@ -49,7 +49,7 @@ export class AppController {
   updateReport(
     @Param('type', ReportTypeValidationPipe) type: string,
     @Param('id', ParseUUIDPipe) id: string,
-    @Body() body: { amount: number; source: string },
+    @Body() body: UpdateReportDto,
   ) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
