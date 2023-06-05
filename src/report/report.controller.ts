a2fb34c4c78378a26e23bb47003c683451acc3f7
kennedy7 <kennedyhillary6@gmail.com>
2023-06-05 23:14:35 +0100

    refactor

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -1,4 +1,67 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Patch,
+  ParseUUIDPipe,
+  ParseEnumPipe,
+} from '@nestjs/common';
+import { ReportType } from '../data';
+import { ReportTypeValidationPipe } from '../pipes/report-type-validation.pipe';
+import { Delete, HttpCode } from '@nestjs/common/decorators';
+import {
+  CreateReportDto,
+  ReportResponseDto,
+  UpdateReportDto,
+} from '../dtos/report.dto';
+import { ReportService } from './report.service';
 
-@Controller('report')
-export class ReportController {}
+@Controller('report/:type')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Get()
+  getAllReports(@Param('type') type: string): ReportResponseDto[] {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.getAllReports(reportType);
+  }
+
+  @Get(':id')
+  getReportById(
+    //alternative way of validating the reportType with an already existing module
+    @Param('type', new ParseEnumPipe(ReportType)) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.getReportById(reportType, id);
+  }
+
+  @Post()
+  createReport(
+    @Body() { amount, source }: CreateReportDto,
+    @Param('type', ReportTypeValidationPipe) type: ReportType,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.createReport(reportType, { amount, source });
+  }
+  @Patch(':id')
+  updateReport(
+    @Param('type', ReportTypeValidationPipe) type: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() body: UpdateReportDto,
+  ): ReportResponseDto {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return this.reportService.updateReport(reportType, id, body);
+  }
+  @HttpCode(204)
+  @Delete(':id')
+  deleteReport(@Param('id', ParseUUIDPipe) id: string) {
+    return this.reportService.deleteReport(id);
+  }
+}
